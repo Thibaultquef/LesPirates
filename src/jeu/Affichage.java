150913f2c1292da5c0393cb492949bbd05c907b9
thibaultquef <thibault.queffelec@univ-tlse3.fr>
2024-04-01 15:28:55 +0200

    correction classe affichage

--- src/jeu/Affichage.java
+++ src/jeu/Affichage.java
@@ -1,33 +1,49 @@
 package jeu;
 
 import CaseSpeciale.Case;
+import Enum.Arme;
 
 public interface Affichage {
-	public void contexte(Pirate listePirate);
-
+	public void afficherDebutDuel(Pirate pirate1, Pirate pirate2);
+	
+	public void afficherResultatDuel(Pirate attaquant, Pirate defenseur, int pvPerdus);
+	
+	public void afficherFinDuel(Pirate attaquant, Pirate defenseur);
+	
+	public void afficherGagnantDuel(Pirate gagnant);
+	
+	public void afficherGagnant(Pirate gagnant);
+	
 	public void aQuiTour(Pirate pirate);
-
-	public void lancerDe(Pirate pirate, int de);
-
-	public void deplacement(Pirate pirate, int de, int nbCase);
-
+	
+	public void lancerDe(Pirate pirate, int valeurDe);
+	
+	public void deplacement(Pirate pirate, int valeurDe, int nbCase);
+	
 	public void descCase(Pirate pirate, Case caseActuelle);
-
-	public void appliquerEffetNeutre(Pirate pirate);
-
-	public void appliquerEffetRhum(Pirate pirate);
-
-	public void appliquerEffetArme(Case caseActuelle, Pirate pirate);
-
-	public void debutDuel(Pirate pirateI, Pirate pirateJ, int deI, int deJ);
-
-	public void finDuel(Pirate gagnant, Pirate perdant, int degat);
-
-	public void gagantBarque(Pirate pirate);
-
-	public void gagantParDuel(Pirate pirate);
-
-	public void tousMort();
-
-	public void aPeri(Pirate pirate);
+	
+	public void gagnantPartie(Pirate pirate);
+	
+	public void afficheTrouveArme(Pirate pirate, Arme arme, int force);
+	
+	public void affichePiratePrendArme(Pirate pirate, Arme arme);
+	
+	public void affichePirateIgnoreArme(Pirate pirate);
+	
+	public void afficherReculeRhum(int lanceDe);
+	
+	public void afficherDebutTour(Pirate pirate);
+	
+	public void afficherLanceDe(int valeurDe);
+	
+	public void afficherDeplacement(Pirate pirate, int valeurDe);
+	
+	public void afficherFinDePartie(int tourActuel);
+	
+	public void afficherInfoCase(Pirate pirate, Case caseActuelle);
+	
+	public void afficherEffetFruit(Pirate pirate);
+	
+	public void afficherDoubleDe(Pirate pirate, int valeurDe);
+	
 }
